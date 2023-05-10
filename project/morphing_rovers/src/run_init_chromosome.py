95671539bf377c9f8c2b8f84e88c3cd6a038df52
Thomas Uriot <thomas.uriot@ncia.nato.int>
2023-05-10 15:39:44 +0200

    stuff for clustering by scenario

--- project/morphing_rovers/src/run_init_chromosome.py
+++ project/morphing_rovers/src/run_init_chromosome.py
@@ -12,7 +12,7 @@ from morphing_rovers.morphing_udp import morphing_rover_UDP, MAX_TIME, Rover
 from morphing_rovers.src.neural_network_supervised.optimization import OptimizeNetworkSupervised
 from utils import init_modes, adjust_clusters_and_modes, update_chromosome_with_mask
 
-PATH_CHROMOSOME = "./trained_chromosomes/chromosome_iteration_0.p"
+PATH_CHROMOSOME = "./trained_chromosomes/chromosome_iteration_1.p"
 N_ITERATIONS_FULL_RUN = 1
 
 
@@ -31,9 +31,10 @@ if __name__ == "__main__":
         rover = Rover(chromosome)
         control = rover.Control
         masks_tensors = [torch.rand(11, 11, requires_grad=True) for _ in range(4)]
-        chromosome = update_chromosome_with_mask(masks_tensors, control.chromosome, always_switch=False)
+        chromosome = update_chromosome_with_mask(masks_tensors, control.chromosome, always_switch=True)
 
     fitness = udp.fitness(chromosome)
+    # udp.plot(chromosome)
     print("initial fitness", fitness, "overall speed", np.mean(udp.rover.overall_speed))
     # initial run to get the dataset for clustering
     masks_tensors, cluster_trainer_output, best_average_speed = init_modes(options, chromosome)
@@ -48,9 +49,9 @@ if __name__ == "__main__":
     fitness = udp.fitness(chromosome)[0]
     print("initial fitness", fitness, "overall speed", np.mean(udp.rover.overall_speed))
 
-    for j in range(100):
-        for n_iter in range(MAX_TIME, MAX_TIME+1):
-            if n_iter % 1 == 0:
+    for j in range(1):
+        for n_iter in range(1, MAX_TIME+1):
+            if n_iter % 10 == 0:
                 print(f"Optimizing network for the {n_iter} first rover's steps")
 
                 network_trainer = OptimizeNetworkSupervised(options, chromosome)
@@ -59,14 +60,14 @@ if __name__ == "__main__":
 
                 chromosome = update_chromosome_with_mask(masks_tensors,
                                                          network_trainer.udp.rover.Control.chromosome,
-                                                         always_switch=False)
+                                                         always_switch=True)
 
                 fitness = udp.fitness(chromosome)[0]
                 print("FITNESS AFTER PATH LEARNING", fitness, "overall speed", np.mean(udp.rover.overall_speed),
                       "average distance from objectives:", np.mean(network_trainer.udp.rover.overall_distance))
 
                 # clustering
-                cluster_trainer = ClusteringTerrain(options, data=path_data, random_state=i)
+                cluster_trainer = ClusteringTerrain(options, data=path_data, random_state=j)
                 cluster_trainer.run()
                 cluster_trainer_output = cluster_trainer.output
 
@@ -83,18 +84,10 @@ if __name__ == "__main__":
                 # updated chromosome
                 chromosome = update_chromosome_with_mask(masks_tensors,
                                                         network_trainer.udp.rover.Control.chromosome,
-                                                         always_switch=False)
-
-                network_trainer = OptimizeNetworkSupervised(options, chromosome)
-                network_trainer.train(n_iter, train=True)
-                path_data = network_trainer.udp.rover.cluster_data
-
-                chromosome = update_chromosome_with_mask(masks_tensors,
-                                                         network_trainer.udp.rover.Control.chromosome,
-                                                         always_switch=False)
+                                                         always_switch=True)
 
                 # compute fitness
-                fitness = udp.fitness(new_chromosome)[0]
+                fitness = udp.fitness(chromosome)[0]
                 print("FITNESS AFTER MODE OPTIMIZATION", fitness, "overall speed", np.mean(udp.rover.overall_speed))
 
 
